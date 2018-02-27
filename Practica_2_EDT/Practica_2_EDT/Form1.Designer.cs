43c027e18d496faf4dbcbcbf697b896be7ab9920
Shernandezglez <32920669+Shernandezglez@users.noreply.github.com>
2018-02-26 21:50:19 -0600

    Add files via upload

---
+++ Practica_2_EDT/Practica_2_EDT/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Practica_2_EDT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblImg = new System.Windows.Forms.Label();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.txtBits = new System.Windows.Forms.TextBox();
+            this.txtAncho = new System.Windows.Forms.TextBox();
+            this.txtAlto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtImg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblImg
+            // 
+            this.lblImg.AutoSize = true;
+            this.lblImg.Location = new System.Drawing.Point(74, 28);
+            this.lblImg.Name = "lblImg";
+            this.lblImg.Size = new System.Drawing.Size(79, 13);
+            this.lblImg.TabIndex = 1;
+            this.lblImg.Text = "Imagenes BMP";
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(12, 253);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(100, 20);
+            this.txtSize.TabIndex = 2;
+            // 
+            // txtBits
+            // 
+            this.txtBits.Location = new System.Drawing.Point(134, 253);
+            this.txtBits.Name = "txtBits";
+            this.txtBits.Size = new System.Drawing.Size(100, 20);
+            this.txtBits.TabIndex = 3;
+            // 
+            // txtAncho
+            // 
+            this.txtAncho.Location = new System.Drawing.Point(12, 321);
+            this.txtAncho.Name = "txtAncho";
+            this.txtAncho.Size = new System.Drawing.Size(100, 20);
+            this.txtAncho.TabIndex = 4;
+            // 
+            // txtAlto
+            // 
+            this.txtAlto.Location = new System.Drawing.Point(134, 321);
+            this.txtAlto.Name = "txtAlto";
+            this.txtAlto.Size = new System.Drawing.Size(100, 20);
+            this.txtAlto.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Size";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(131, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Bits por Pixel";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 305);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ancho";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(131, 305);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Alto";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(286, 133);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 10;
+            this.btnCalc.Text = "Seleccionar Imagen";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtImg
+            // 
+            this.txtImg.Location = new System.Drawing.Point(15, 44);
+            this.txtImg.Multiline = true;
+            this.txtImg.Name = "txtImg";
+            this.txtImg.Size = new System.Drawing.Size(219, 181);
+            this.txtImg.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 354);
+            this.Controls.Add(this.txtImg);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAlto);
+            this.Controls.Add(this.txtAncho);
+            this.Controls.Add(this.txtBits);
+            this.Controls.Add(this.txtSize);
+            this.Controls.Add(this.lblImg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblImg;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.TextBox txtBits;
+        private System.Windows.Forms.TextBox txtAncho;
+        private System.Windows.Forms.TextBox txtAlto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtImg;
+    }
+}
+
---
+++ Practica_2_EDT/Practica_2_EDT/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_2_EDT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialogo = new OpenFileDialog();
+
+            byte[] bmp = { 0x42, 0x4D };
+
+            dialogo.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            dialogo.Filter = "Imagenes (*.bmp)|";
+
+            dialogo.RestoreDirectory = true;
+
+            
+
+            if (dialogo.ShowDialog() == DialogResult.OK) 
+            {
+                byte img = Convert.ToByte(dialogo.FileName);
+                byte[] file = { img };
+
+                if (file == bmp)
+                {
+                    txtImg.Text = dialogo.SafeFileName;
+                }
+            }
+            else
+            {
+                 MessageBox.Show ("Seleccione una imapeg tipo BMP", "Advertencia");
+            }
+            
+            
+        }
+
+        /*public byte getBytesOfBitMap()
+        {
+
+        }*/
+    }
+}
